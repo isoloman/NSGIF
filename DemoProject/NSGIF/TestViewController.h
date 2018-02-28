c65cb58248586eb37915805c9b92fd8f3066c361
殷亿程 <89932996@qq.com>
2018-02-28 16:50:49 +0800

    添加测试VC

---
+++ DemoProject/NSGIF/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  NSGIF
+//
+//  Created by admin on 2018/2/28.
+//  Copyright © 2018年 Sebastian Dobrincu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
